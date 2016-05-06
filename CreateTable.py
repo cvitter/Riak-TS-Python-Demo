5b2adc1da2bfd684c4965b562fa2cac69b5c948d
Craig Vitter <craig.vitter@gmail.com>
2016-05-06 07:51:47 -0400

    Update CreateTable code to reflect more flexible primary key designation added in Riak TS 1.3

--- CreateTable.py
+++ CreateTable.py
@@ -5,6 +5,11 @@ __doc__ = """\
 CreateTable.py
 Demonstrates how to create a table in Riak TS via the Python Client API
 
+V1.3: Updated to reflect additional flexibility in designating primary
+      keys. As of 1.3 only the quantum function is required (as shown
+      below) however it is still possible to have multi-part primary
+      keys as long as the quantum is the last part of the key specified.
+
 For more information see the Python Client API documentation at:
     http://docs.basho.com/riakts/latest/developing/python/
 """
@@ -20,8 +25,8 @@ CREATE TABLE waterMeterData (
     gallons_per_hour  double    not null,
     total_gallons     double    not null,
     PRIMARY KEY(
-        (customer_id, meter_id, quantum(ts, 30, 'd')),
-         customer_id, meter_id, ts
+        ( quantum(ts, 30, 'd') ),
+          ts
     )
 )
 """
